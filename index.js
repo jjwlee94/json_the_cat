655416e837e949673ad442d7aef1d56a053a2d3f
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-05 01:05:40 +0000

    Refactor

---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, description) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+    return;
+  } else {
+    console.log(description);
+  }
+});
