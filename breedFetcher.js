6c5d5f121d7b63658728a4069e9ff131609eacb9
Jeewon Lee <jeewon.jlee@gmail.com>
2021-11-05 01:05:12 +0000

    Refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,21 @@
 const request = require("request");
 
-const breed = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0] !== undefined) {
+      callback(null, data[0].description);
+      return;
+    } else {
+      callback("ERROR: BREED NOT FOUND", null);
+      return;
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    return console.log("ERROR");
-  }
-  const data = JSON.parse(body);
-  if (data[0] !== undefined) {
-    console.log(data);
-  } else {
-    console.log("ERROR: BREED NOT FOUND");
-  }
-});
+
+module.exports = { fetchBreedDescription };
